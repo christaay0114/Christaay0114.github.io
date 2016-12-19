24d269e53bbaab789e8f59e58fe6ac67e6aa2fb9
Christina's <mariarestrepo@Marias-MacBook-Pro-4.local>
2016-12-18 23:01:16 -0500

    changed main page to the yellow page

--- non-linear-narrative/yellowpage/popup.js
+++
@@ -1,9 +0,0 @@
-
-<script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-    var popup = document.getElementById('myPopup');
-    popup.classList.toggle('show');
-}
-</script>
-Try it Yourself »
